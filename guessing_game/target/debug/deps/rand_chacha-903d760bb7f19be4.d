30a5bf379cc6c9dacc388c8ee70261702696344f
Uzo-chi <uzochiokoronkwo.official@gmail.com>
2023-11-15 11:08:55 +0100

    (A guessing game with rust)

---
+++ guessing_game/target/debug/deps/rand_chacha-903d760bb7f19be4.d
@@ -0,0 +1,9 @@
+C:\Users\franc\Documents\projects\guessing_game\target\debug\deps\rand_chacha-903d760bb7f19be4.rmeta: C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\franc\Documents\projects\guessing_game\target\debug\deps\librand_chacha-903d760bb7f19be4.rlib: C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\franc\Documents\projects\guessing_game\target\debug\deps\rand_chacha-903d760bb7f19be4.d: C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs
+
+C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\lib.rs:
+C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\chacha.rs:
+C:\Users\franc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\rand_chacha-0.3.1\src\guts.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\franc\Documents\projects\guessing_game\target\debug\guessing_game.exe: C:\Users\franc\Documents\projects\guessing_game\src\main.rs
